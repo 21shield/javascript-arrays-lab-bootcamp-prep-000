0c45af29a0149c64b64f3612916be704d27ac30d
Netaly Ramirez <21netaly@gmail.com>
2020-03-15 23:06:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ function removeLastKitten(){
   return babyCats
 }
  function removeFirstKitten(){
-   let babyCats = kittens.splice()
+   let babyCats = kittens.slice(1,kittens.length)
    return babyCats
  }
