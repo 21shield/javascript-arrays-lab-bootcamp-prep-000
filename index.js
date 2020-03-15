ed54d547f0572536ea3d21c36b47eb2f58a03d8f
Netaly Ramirez <21netaly@gmail.com>
2020-03-15 22:51:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 var kittens = ['Milo','Otis','Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten () {
+  kittens.shift
+  return kittens
+}
+function apendKitten(...kittens,name){
+}
+function(){}
+function(){}
+function(){}
